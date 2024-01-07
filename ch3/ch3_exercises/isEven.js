a663a65ed7f9de1a284d652c7a6deb5efd475922
Hayden <hayden.garry99@gmail.com>
2024-01-06 21:03:56 -0500

    .

---
+++ ch3/ch3_exercises/isEven.js
@@ -0,0 +1,8 @@
+function isEven(num) {
+  if (num % 2 === 0) {
+    return "That number is even.";
+  } else {
+    return "That number is odd.";
+  }
+}
+console.log("log #1:", isEven(75));
---
+++ ch3/ch3_exercises/minimum.js
@@ -0,0 +1,5 @@
+function min(arg1, arg2) {
+  return Math.min(arg1, arg2);
+}
+console.log("log #1 minimum is:", min(0, 10));
+console.log("log #2 minimum is:", min(4, 10));
